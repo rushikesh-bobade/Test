9e0e8dd0e843e15f3e44c19386b87e0c3db62b83
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-07-07 20:58:58 +0530

    ✅ Print the Elements of a Linked List | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-07

---
+++ hackerrank/medium/print-the-elements-of-a-linked-list/solution.cpp
@@ -0,0 +1,22 @@
+
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* current = head;
+
+    while (current != nullptr) {
+        cout << current->data << "\n";
+        current = current->next;
+    }
+}
+
